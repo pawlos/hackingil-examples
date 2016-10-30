87665197be278df513926443479b1a49f9e9040b
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2016-10-30 23:17:55 +0100

    Emit byte demo

---
+++ emitbyte/emitbyte.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace emitbyte
+{
+    class emitbyte
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Secret string");
+        }
+    }
+}
