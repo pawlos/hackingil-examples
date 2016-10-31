1d6072c086c635f04b5a55988de3f106757058b8
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2016-10-31 23:26:38 +0100

    obfuscation example

---
+++ obfuscate/obfuscate.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace obfuscate
+{
+    class obfuscate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello obfusacate");
+        }
+
+        static int Method1()
+        {
+            return 0;
+        }
+    }
+}
