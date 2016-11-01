b30db17aaa0b5922ed43b39c5ca3f60e07631735
pawlos <lukasik.pawel+github@gmail.com>
2016-11-01 20:36:04 +0100

    module inject example

---
+++ module-inject/module_inject.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace module_inject
+{
+	public class Initializer
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Main");
+			new Initializer().Init();
+		}
+
+		static Initializer()
+		{
+			Console.WriteLine("Initializer cctor()");
+		}
+
+		public Initializer()
+		{
+			Console.WriteLine("Initializer ctor()");
+		}
+
+		public void Init()
+		{
+			Console.WriteLine("Initialize.Init()");
+		}
+	}
+
+
+	public class Cache
+	{
+		static Cache()
+		{
+			Console.WriteLine("Inside Cache .cctor");
+		}
+
+		public Cache() 
+		{
+			Console.WriteLine("Inside Cache .ctor");
+		}
+	}
+}
